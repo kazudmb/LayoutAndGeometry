27a4baf9bd0151d341f48a497c5e998bd98096ef
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-08-11 09:21:31 +0900

    add Project 18, part 1
    https://www.hackingwithswift.com/100/swiftui/92

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,10 +8,37 @@
 
 import SwiftUI
 
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack(alignment: .midAccountAndName) {
+            
+            VStack {
+                Text("@twostraws")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                Image("paul-hudson")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("Full name:")
+                Text("PAUL HUDSON")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                    .font(.largeTitle)
+            }
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
